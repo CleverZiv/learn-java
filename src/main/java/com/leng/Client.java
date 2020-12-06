fe6bf7e0c64a8baa4629d67ba97bca3f5c096f91
CleverZiv <13693371454@163.com>
2020-12-06 23:59:17 +0800

    the first commit project

---
+++ src/main/java/com/leng/Client.java
@@ -0,0 +1,12 @@
+package com.leng;
+
+/**
+ * @Classname Client
+ * @Date 2020/12/6 23:56
+ * @Autor lengxuezhang
+ */
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
